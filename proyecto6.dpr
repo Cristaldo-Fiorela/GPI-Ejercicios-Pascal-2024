e52341c739f4e239137096c9b4b9957d675ef7cc
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-04-11 23:46:40 -0300

    correccion de sintaxis y variables de ejercicio 6

--- proyecto6.dpr
+++ proyecto6.dpr
@@ -3,12 +3,7 @@ program proyecto6;
 {$APPTYPE CONSOLE}
 
   var
-    num1: Integer;
-    num2: Integer;
-    num3: Integer;
-    num4: Integer;
-    suma: Integer;
-    producto: Integer;
+    num1, num2, num3, num4, suma, producto: Integer;
 
   begin
     Write('Ingrese su primer numero: ');
@@ -26,11 +21,8 @@ program proyecto6;
     suma := num1 + num2;
     producto := num3 * num4;
 
-    Write('La suma de los dos primeros numeros es: ');
-    WriteLn(suma);
-
-    Write('El producto de los dos ultimos numeros es: ');
-    WriteLn(producto);
+    Write('La suma de los dos primeros numeros es: ', suma);
+    Write('El producto de los dos ultimos numeros es: ', producto);
 
     ReadLn;
   end.
